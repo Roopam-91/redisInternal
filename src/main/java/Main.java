fd031d92471c0adbeb88639972aab14b7c4cb74e
RoopamSamal <roopam@ibcscorp.com>
2024-05-06 09:20:47 +0530

    handle concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,41 +12,45 @@ public class Main {
 
         //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
         try {
+            // Wait for connection from client.
+            ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
             serverSocket = new ServerSocket(port);
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            InputStream input = clientSocket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(input);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            OutputStream output = clientSocket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(output);
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-                if (line.contains("ping")) {
-                    bufferedWriter.write("+PONG\r\n");
-                }
-                bufferedWriter.flush();
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                executor.submit(() -> {
+                    try {
+                        handleRequest(clientSocket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
 
 
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private static void handleRequest(Socket clientSocket) throws IOException {
+        InputStream input = clientSocket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(input);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        OutputStream output = clientSocket.getOutputStream();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(output);
+        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+            if (line.contains("ping")) {
+                bufferedWriter.write("+PONG\r\n");
             }
+            bufferedWriter.flush();
         }
     }
 }
