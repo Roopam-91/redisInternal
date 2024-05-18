05775375d2c2ae9465a3c34e4cf550ff141ca16a
RoopamSamal <roopam@ibcscorp.com>
2024-05-18 19:09:18 +0530

    replication

--- src/main/java/ReplicaRequestHandler.java
+++ src/main/java/ReplicaRequestHandler.java
@@ -28,17 +28,24 @@ public class ReplicaRequestHandler implements RequestHandler {
             while (true) {
                 clientSocket.getOutputStream().write("*1\r\n$4\r\nPING\r\n".getBytes(StandardCharsets.UTF_8));
                 clientSocket.getOutputStream().flush();
-                String response1 = new String(clientSocket.getInputStream().readAllBytes());
-                System.out.println("Response1 " + response1);
+//                InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+//                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                byte[] buffer = new byte[1024];
+                int bytesRead = clientSocket.getInputStream().read(buffer);
+                String response = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
+                System.out.println("Response1 " + response);
                 clientSocket.getOutputStream().write(("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n" + port + "\r\n")
                         .getBytes(StandardCharsets.UTF_8));
-                clientSocket.getOutputStream().flush();
-                String response2 = new String(clientSocket.getInputStream().readAllBytes());
-                System.out.println("Response2 " + response2);
+                byte[] buffer2 = new byte[1024];
+                int bytesRead2 = clientSocket.getInputStream().read(buffer2);
+                String response2 = new String(buffer2, 0, bytesRead2, StandardCharsets.UTF_8);
+                System.out.println("Response1 " + response2);
                 clientSocket.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".getBytes(StandardCharsets.UTF_8));
                 clientSocket.getOutputStream().flush();
-                String response = new String(clientSocket.getInputStream().readAllBytes());
-                System.out.println("Response3 " + response);
+                byte[] buffer3 = new byte[1024];
+                int bytesRead3 = clientSocket.getInputStream().read(buffer3);
+                String response3 = new String(buffer3, 0, bytesRead3, StandardCharsets.UTF_8);
+                System.out.println("Response1 " + response3);
                 if (response.contains("OK")) {
                     clientSocket.getOutputStream().write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".getBytes(StandardCharsets.UTF_8));
                     clientSocket.getOutputStream().flush();
