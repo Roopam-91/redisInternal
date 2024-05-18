e3bf733f2dee6a66272dca94b88217c92ce1e3ae
RoopamSamal <roopam@ibcscorp.com>
2024-05-18 16:40:45 +0530

    replication

--- src/main/java/ReplicaRequestHandler.java
+++ src/main/java/ReplicaRequestHandler.java
@@ -25,14 +25,19 @@ public class ReplicaRequestHandler implements RequestHandler {
             clientSocket = new Socket("localhost", 6379);
             clientSocket.setReuseAddress(true);
             // Send a message to the server
-            clientSocket.getOutputStream().write("*1\r\n$4\r\nPING\r\n".getBytes(StandardCharsets.UTF_8));
-            clientSocket.getOutputStream().write(("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n"+port+"\r\n")
-                    .getBytes(StandardCharsets.UTF_8));
-            clientSocket.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".getBytes(StandardCharsets.UTF_8));
-            String response = new String(clientSocket.getInputStream().readAllBytes());
-            System.out.println("Response " + response);
-            clientSocket.getOutputStream().write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".getBytes(StandardCharsets.UTF_8));
-            clientSocket.getOutputStream().flush();
+            while (true) {
+                clientSocket.getOutputStream().write("*1\r\n$4\r\nPING\r\n".getBytes(StandardCharsets.UTF_8));
+                clientSocket.getOutputStream().write(("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n" + port + "\r\n")
+                        .getBytes(StandardCharsets.UTF_8));
+                clientSocket.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".getBytes(StandardCharsets.UTF_8));
+                String response = new String(clientSocket.getInputStream().readAllBytes());
+                System.out.println("Response " + response);
+                if (response.contains("OK")) {
+                    clientSocket.getOutputStream().write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".getBytes(StandardCharsets.UTF_8));
+                }
+                clientSocket.getOutputStream().flush();
+                break;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
