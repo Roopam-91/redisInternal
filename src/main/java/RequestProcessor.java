04d175239b33514237c6d2f5af9e5dc57b31d315
RoopamSamal <roopam@ibcscorp.com>
2024-05-20 09:16:09 +0530

    command

--- src/main/java/RequestProcessor.java
+++ src/main/java/RequestProcessor.java
@@ -132,8 +132,8 @@ public class RequestProcessor {
                     System.out.println("Sending to replicas");
                     socket.getOutputStream().write(request.getBytes(StandardCharsets.UTF_8));
                     socket.getOutputStream().flush();
-                    String response = ReplicaRequestHandler.getResponse(socket);
-                    System.out.println(response);
+//                    String response = ReplicaRequestHandler.getResponse(socket);
+//                    System.out.println(response);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
