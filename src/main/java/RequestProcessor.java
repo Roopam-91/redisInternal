f6486406e445f04484fc2bae724165d387405bf7
RoopamSamal <roopam@ibcscorp.com>
2024-05-22 10:11:32 +0530

    redis

--- src/main/java/RequestProcessor.java
+++ src/main/java/RequestProcessor.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -48,93 +46,84 @@ public class RequestProcessor {
     public void handleRequest(Socket clientSocket) {
         try {
             while (clientSocket.isConnected()) {
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-                String command = "";
-                String currLine;
-                while ((currLine = reader.readLine()) != null) {
-                    command += currLine;
-                    if (command.startsWith("*")) {
-                        System.out.println("command -> " +command);
-                        System.out.println(storage);
-                        String request = command.trim();
-                        String[] parts = request.split("\r\n");
-                        System.out.println(Arrays.toString(parts));
-                        if (parts.length >= 2) {
-                            if (parts[2].equalsIgnoreCase("SET")) {
-                                String key = parts[4];
-                                String value = parts[6];
-                                long timeout = parts.length == 11 ? Long.parseLong(parts[10]) : 0;
-                                Data data = new Data(value, timeout);
-                                storage.put(key, data);
-                                if (Role.REPLICA.name().equals(role.name())) {
-                                    System.out.println(role + " --> " + storage);
-                                }
-                                if (Role.MASTER.name().equals(role.name())) {
-                                    String response = "OK";
-                                    clientSocket.getOutputStream().write(("$" + response.length() + "\r\n" + response + "\r\n")
-                                            .getBytes());
-                                    sendToReplicas(command);
-                                }
-                            } else if (parts[2].equalsIgnoreCase("GET")) {
-                                Object rawData = storage.get(parts[4]);
-                                String value = null;
-                                if (Objects.nonNull(rawData)) {
-                                    Data data = (Data) rawData;
-                                    if (data.expiry > 0 && data.expiry < System.currentTimeMillis()) {
-                                        storage.remove(parts[4]);
-                                    } else {
-                                        value = (String) data.value;
-                                    }
-                                }
-                                if (Role.REPLICA.name().equals(role.name())) {
-                                    System.out.println(role + " --> " + storage);
-                                }
-                                if (Objects.nonNull(value)) {
-                                    clientSocket.getOutputStream().write(
-                                            ("$" + value.length() + "\r\n" + value + "\r\n").getBytes());
-                                } else {
-                                    clientSocket.getOutputStream().write(
-                                            ("$-1\r\n").getBytes());
-                                }
-                            } else if (parts[2].equalsIgnoreCase("INFO")) {
-                                StringBuilder builder = new StringBuilder();
-                                infoMap.forEach((key, value) -> builder.append(key).append(":").append(value));
-                                String value = builder.toString();
-                                clientSocket.getOutputStream().write(
-                                        ("$" + value.length() + "\r\n" + value + "\r\n").getBytes());
-                            } else if (parts[2].equalsIgnoreCase("REPLCONF") && Role.MASTER.name().equals(role.name())) {
-                                String data = "OK";
-                                clientSocket.getOutputStream().write(
-                                        ("$" + data.length() + "\r\n" + data + "\r\n").getBytes());
-                            } else if (parts[2].equalsIgnoreCase("PSYNC") && Role.MASTER.name().equals(role.name())) {
-                                String replID = REPL_ID + UUID.randomUUID().toString().substring(25);
-                                String data = String.format("+FULLRESYNC %s %d%s", replID, OFFSET, "\r\n");
-                                clientSocket.getOutputStream().write(data.getBytes());
-                                clientSocket.getOutputStream().flush();
-                                String fileContents = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
-                                byte[] bytes = Base64.getDecoder().decode(fileContents);
-                                clientSocket.getOutputStream().write(("$" + bytes.length + "\r\n").getBytes());
-                                clientSocket.getOutputStream().flush();
-                                clientSocket.getOutputStream().write(bytes);
-                                clientSocket.getOutputStream().flush();
-                                replicaMap.put(replID, clientSocket);
-                            } else if (parts[2].equalsIgnoreCase("ECHO")) {
-                                String data = parts[4];
-                                clientSocket.getOutputStream().write(
-                                        ("$" + data.length() + "\r\n" + data + "\r\n").getBytes());
-                            } else if (parts[2].equalsIgnoreCase("PING")) {
-                                System.out.println("Sending Pong....");
-                                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                byte[] input = new byte[1024];
+                int bytesRead = clientSocket.getInputStream().read(input);
+                String rawRequest = new String(input, 0, bytesRead);
+                String request = rawRequest.trim();
+                String[] parts = request.split("\r\n");
+                if (parts.length >= 2) {
+                    if (parts[2].equalsIgnoreCase("SET")) {
+                        String key = parts[4];
+                        String value = parts[6];
+                        long timeout = parts.length == 11 ? Long.parseLong(parts[10]) : 0;
+                        Data data = new Data(value, timeout);
+                        storage.put(key, data);
+                        if (Role.REPLICA.name().equals(role.name())) {
+                            System.out.println(role + " --> " + storage);
+                        }
+                        if (Role.MASTER.name().equals(role.name())) {
+                            String response = "OK";
+                            clientSocket.getOutputStream().write(("$" + response.length() + "\r\n" + response + "\r\n")
+                                    .getBytes());
+                            sendToReplicas(rawRequest);
+                        }
+                    } else if (parts[2].equalsIgnoreCase("GET")) {
+                        Object rawData = storage.get(parts[4]);
+                        String value = null;
+                        if (Objects.nonNull(rawData)) {
+                            Data data = (Data) rawData;
+                            if (data.expiry > 0 && data.expiry < System.currentTimeMillis()) {
+                                storage.remove(parts[4]);
                             } else {
-                                clientSocket.getOutputStream().write(
-                                        "-ERR invalid request\r\n".getBytes());
+                                value = (String) data.value;
                             }
+                        }
+                        if (Role.REPLICA.name().equals(role.name())) {
+                            System.out.println(role + " --> " + storage);
+                        }
+                        if (Objects.nonNull(value)) {
+                            clientSocket.getOutputStream().write(
+                                    ("$" + value.length() + "\r\n" + value + "\r\n").getBytes());
                         } else {
                             clientSocket.getOutputStream().write(
-                                    "-ERR invalid request\r\n".getBytes());
+                                    ("$-1\r\n").getBytes());
                         }
+                    } else if (parts[2].equalsIgnoreCase("INFO")) {
+                        StringBuilder builder = new StringBuilder();
+                        infoMap.forEach((key, value) -> builder.append(key).append(":").append(value));
+                        String value = builder.toString();
+                        clientSocket.getOutputStream().write(
+                                ("$" + value.length() + "\r\n" + value + "\r\n").getBytes());
+                    } else if (parts[2].equalsIgnoreCase("REPLCONF") && Role.MASTER.name().equals(role.name())) {
+                        String data = "OK";
+                        clientSocket.getOutputStream().write(
+                                ("$" + data.length() + "\r\n" + data + "\r\n").getBytes());
+                    } else if (parts[2].equalsIgnoreCase("PSYNC") && Role.MASTER.name().equals(role.name())) {
+                        String replID = REPL_ID + UUID.randomUUID().toString().substring(25);
+                        String data = String.format("+FULLRESYNC %s %d%s", replID, OFFSET, "\r\n");
+                        clientSocket.getOutputStream().write(data.getBytes());
+                        clientSocket.getOutputStream().flush();
+                        String fileContents = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
+                        byte[] bytes = Base64.getDecoder().decode(fileContents);
+                        clientSocket.getOutputStream().write(("$" + bytes.length + "\r\n").getBytes());
+                        clientSocket.getOutputStream().flush();
+                        clientSocket.getOutputStream().write(bytes);
+                        clientSocket.getOutputStream().flush();
+                        replicaMap.put(replID, clientSocket);
+                    } else if (parts[2].equalsIgnoreCase("ECHO")) {
+                        String data = parts[4];
+                        clientSocket.getOutputStream().write(
+                                ("$" + data.length() + "\r\n" + data + "\r\n").getBytes());
+                    } else if (parts[2].equalsIgnoreCase("PING")) {
+                        System.out.println("Sending Pong....");
+                        clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                    } else {
+                        clientSocket.getOutputStream().write(
+                                "-ERR invalid request\r\n".getBytes());
                     }
+                } else {
+                    clientSocket.getOutputStream().write(
+                            "-ERR invalid request\r\n".getBytes());
                 }
             }
         } catch (IOException e) {
